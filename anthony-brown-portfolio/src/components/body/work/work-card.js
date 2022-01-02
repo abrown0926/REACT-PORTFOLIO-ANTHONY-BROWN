d25cbebfb6cbb618f0d1ebebc2e5a333f698cb46
AQB <abrown0926>
2022-01-02 17:42:54 -0600

    updated portfolio

--- anthony-brown-portfolio/src/components/body/work/work-card.js
+++ anthony-brown-portfolio/src/components/body/work/work-card.js
@@ -5,12 +5,14 @@ function WorkCard({ item }) {
   return (
     <div className="work-card">
       <img alt="company logo" src={item.companyLogo} className="work-logo" />
-      <label className="company-name">{item.company}</label>
-      <div className="work-dates">
-        <label>{item.dateJoining}</label>-<label>{item.dateEnd}</label>
-      </div>
-      <div className="work-desc">
-        <p>{item.work}</p>
+      <div className="work-info">
+        <label className="company-name">{item.company}</label>
+        <div className="work-dates">
+          <label>{item.dateJoining}</label>-<label>{item.dateEnd}</label>
+        </div>
+        <div className="work-desc">
+          <p>{item.work}</p>
+        </div>
       </div>
     </div>
   );
--- anthony-brown-portfolio/src/components/data/work.js
+++ anthony-brown-portfolio/src/components/data/work.js
@@ -1,10 +1,10 @@
 export const WorkData = [
   {
-    company: "google",
-    designation: "Software Engineer",
-    dateJoining: "02-02-2015",
-    dateEnd: "Present",
-    companyLogo: "",
-    work: "work info",
+    company: "United States Marshal Service",
+    designation: "Deputy United States Marshal",
+    dateJoining: "07/07/2007",
+    dateEnd: "11/11/2015",
+    companyLogo: require("../../assets/marshallogo.png"),
+    work: "Included responsibilities were Judicial Security,  Fugitive Operations, Witness Security, Prisoner Operations, Asset Forfeiture, Justice Prisoner and Alien Transportation System, and Service of Court Process.",
   },
 ];
